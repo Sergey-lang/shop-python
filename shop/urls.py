1bc061a5776e9265c3595e6921d082d9aba3dffc
Siarhei <siarhei.kukharonak@startmatter.com>
2023-10-30 22:42:34 +0300

    add product detail add static folder

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-from shop.views import home
+from shop.views import home, product_list_by_category, product_detail
 urlpatterns = [
     path('', home, name='home'),
+    path('/category/<int:category_pk>', product_list_by_category, name='product_list_by_category'),
+    path('/category/product/<int:product_pk>', product_detail, name='product_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,39 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from shop.models import Category, Product
+
+
+def get_all_categories():
+    return Category.objects.all()
+
+
+def get_products_list(key=None):
+    if key:
+        return Product.objects.filter(category=key)
+    return Product.objects.all()
+
+
+def get_product_detail(key):
+    return get_object_or_404(Product, pk=key)
 
 
 def home(request):
-    context = {"categories": []}
-    return render(request, 'shop/shop.html', context)
+    categories = get_all_categories()
+    products = get_products_list()
+    context = {"categories": categories, 'products': products}
+    return render(request, 'shop/home.html', context)
+
+
+def product_list_by_category(request, category_pk):
+    products_by_category = get_products_list(category_pk)
+    categories = get_all_categories()
+    context = {"categories": categories, 'products': products_by_category}
+    return render(request, 'shop/home.html', context)
+
+
+def product_detail(request, product_pk):
+    product = get_product_detail(product_pk)
+    categories = get_all_categories()
+    context = {"categories": categories, 'product': product}
+    return render(request, 'shop/product_detail.html', context)
